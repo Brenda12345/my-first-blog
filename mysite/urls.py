d7e48cb1997ede0f28eea81e45a91066731b02e2
Brenda12345 <brendainfotec@gmail.com>
2019-06-06 16:42:22 -0500

    Extensiones Django-Python

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path, include
+from django.urls import include, path
 from django.contrib import admin
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/login/', views.login, name='login'),
+    path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
     path('', include('blog.urls')),
 ]
