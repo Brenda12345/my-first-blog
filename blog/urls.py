62bc85ad676694307c21236a9599028507ec7c7d
Brenda12345 <brendainfotec@gmail.com>
2019-06-07 11:50:17 -0500

    Blog casi, casi final

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
@@ -9,5 +10,7 @@ urlpatterns = [
     path('post/<pk>/publish/', views.post_publish, name='post_publish'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('post/<pk>/remove/', views.post_remove, name='post_remove'),
-
+    path('post/<pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/(<pk>)/approve/', views.comment_approve, name='comment_approve'),
+    path('comment/(<pk>)/remove/', views.comment_remove, name='comment_remove'),
 ]
